c35fe711272bdf3647ad124e846d6b1c97ea8f94
Richard Poelderl <richard.poelderl@gmail.com>
2022-09-28 11:28:45 +0200

    fix whatever weird structure I created w/ nest cli

--- apps/server/test/app.e2e-spec.ts
+++ apps/server/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ServerModule } from './../src/server.module';
+import { AppModule } from '../src/app.module';
 
-describe('ServerController (e2e)', () => {
+describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ServerModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
